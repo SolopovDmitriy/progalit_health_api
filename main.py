15ebd753e77e0619841a54ebc95d38f3d327dd2d
Dmitriy Solopov <div.solopov@gmail.com>
2024-05-08 13:37:53 +0300

    save steps to db

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from typing import Union, List
+from typing import Optional, List
+import logging
+import sys
 
 from os import getenv, path
 from dotenv import load_dotenv
 
 from datetime import datetime, timedelta
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
-from models import Base, User
+from models import Base, User, Step
 
 
 load_dotenv()
@@ -23,20 +25,17 @@ engine = create_engine(f"mysql+mysqlconnector://{db_config['user']}:{db_config['
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
-
-
 class UserRequest(BaseModel):
 	username: str
 	password: str
 
-class SyncRequest(BaseModel):
-	userid: str
-	data: int
-
-# class SyncRequest(BaseModel):
-# 	userid: str
-# 	data: Union[str, List[str]]
-
+class StepRequest(BaseModel):
+	startTime: datetime
+	startZoneOffset: Optional[str] = None 
+	endTime: datetime
+	endZoneOffset: Optional[str] = None 
+	count: int
+	stepMetadata: Optional[str] = None
 
 def find_or_insert_user(username: str, password: str) -> bool:
 	try:
@@ -53,27 +52,95 @@ def find_or_insert_user(username: str, password: str) -> bool:
 	return None
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.get("/")
 async def root():
 	return {"message": "Hello World!!!"}
 
 
-@app.post("/api/sync/{method}")
-async def sync(method: str, request: SyncRequest):
+
+def fetch_all_steps(session):
+	return session.query(Step).all()
+
+
+# def step_exists(steps, step):
+# 	for existing_step in steps:
+# 		logger.info(str(type(existing_step.startTime)) + " <======> " + str(type(step.startTime)))
+# 		if (existing_step.startTime == step.startTime and existing_step.endTime == step.endTime):
+# 			return True
+# 	return False
+
+def step_exists(steps, step):
+	for existing_step in steps:		
+		if (existing_step.stepMetadata == step.stepMetadata):
+			return True
+	return False
+
+
+@app.post("/api/sync/steps")
+async def sync_steps(request: List[StepRequest]):
 	try:
-		# method = method[0].lower() + method[1:]
-		print(method)
-		method_str = str(method)	
-		userid = request.userid
-		data = request.data
-		if type(data) != list:
-			data = [data]
-		print(data)
-		return method_str
+		with Session() as session:            
+			existing_steps = fetch_all_steps(session)
+			# logger.info("existing_steps ======> " + str(existing_steps))
+			for step_request in request:
+				step = Step(
+					startTime=step_request.startTime,
+					startZoneOffset=step_request.startZoneOffset,
+					endTime=step_request.endTime,
+					endZoneOffset=step_request.endZoneOffset,
+					count=step_request.count,
+					stepMetadata=step_request.stepMetadata
+				)                
+				if not step_exists(existing_steps, step):
+					session.add(step)
+					session.flush()
+			session.commit() 
+			return {"message": "Steps saved successfully."}
 	except Exception as e:
 		print(f"An error occurred: {e}")
 		return {"error": str(e)}
+
+
+
+
+
+
+
+
+
+
+
+# def saveStep(step: Step):
+# 	try:
+# 		with Session() as session:			
+# 			session.add(step)
+# 			session.flush()			
+# 			session.commit()
+# 			return step
+# 	except Exception as e:
+# 		print(f"An error occurred: {e}")
+# 	return None
+
+# @app.post("/api/sync/steps")
+# async def syncSteps(request: StepRequest):
+# 	try:
+# 		step = Step(
+# 			startTime=request.startTime,
+# 			startZoneOffset=request.startZoneOffset,
+# 			endTime=request.endTime,
+# 			endZoneOffset=request.endZoneOffset,
+# 			count=request.count,
+# 			stepMetadata=request.stepMetadata 
+# 		)
+# 		print(step)
+# 		saveStep(step)
+# 		return {"message": "Step saved successfully."}
+# 	except Exception as e:
+# 		print(f"An error occurred: {e}")
+# 		return {"error": str(e)}
 
 
 @app.post("/api/login")
@@ -91,13 +158,3 @@ def login(request_user: UserRequest):
 		print(f"An error occurred: {e}")
 		raise HTTPException(status_code=400, detail="exception in try")
 
-
-
-#  LOG  5
-#  WARN  [AsyncStorage] The value for key "session" is not a string. This can lead to unexpected behavior/errors. 
-# Consider stringifying it.
-# Passed value: 5
-# Passed key: session
-#  WARN  Possible Unhandled Promise Rejection (id: 17):
-# Error: java.lang.Double cannot be cast to java.lang.String
-# Error: java.lang.Double cannot be cast to java.lang.String
--- models.py
+++ models.py
@@ -8,7 +8,26 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user' 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(255), unique=True) 
-    password = Column(String(2500)) 
+	__tablename__ = 'user' 
+	id = Column(Integer, primary_key=True)
+	username = Column(String(255), unique=True) 
+	password = Column(String(2500)) 
+
+
+class Step(Base):
+	__tablename__ = 'step'
+	id = Column(Integer, primary_key=True)
+	startTime = Column(DateTime)
+	startZoneOffset = Column(String(20)) 
+	endTime = Column(DateTime)
+	endZoneOffset = Column(String(20)) 
+	count = Column(Integer)
+	stepMetadata  = Column(String(2500)) 
+
+	def __init__(self, startTime, startZoneOffset, endTime, endZoneOffset, count, stepMetadata ):
+		self.startTime = startTime
+		self.startZoneOffset = startZoneOffset
+		self.endTime = endTime
+		self.endZoneOffset = endZoneOffset
+		self.count = count
+		self.stepMetadata = stepMetadata 
